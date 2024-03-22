2874c51089b7b78158728dc3bee3a8362d0f5a8f
JaviTrek <j1a1b1m1@gmail.com>
2024-03-22 12:13:05 -0400

    mongo connected but not really

--- app/messages/page.jsx
+++ app/messages/page.jsx
@@ -1,4 +1,21 @@
+"use client"
+import {useEffect} from "react";
+
+
 export default function Page() {
+
+    useEffect(async () => {
+        console.log("trying to connect")
+        const res = await fetch("http://localhost:3000/api/database")
+        const data = await res.json()
+        console.log(data)
+        console.log(Response.json({ data }))
+        //return Response.json({ data })
+
+
+    }, []);
+
+
     return (
         <h1>Page with slug!</h1>
     )
