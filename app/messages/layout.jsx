c5f8e0f26fa0fa8d30bbc6cf8b3f82e9c3aa751c
JaviTrek <j1a1b1m1@gmail.com>
2024-04-08 10:22:19 -0400

    update

--- app/messages/layout.jsx
+++ app/messages/layout.jsx
@@ -1,6 +1,4 @@
-
 async function callMongoDB()  {
-
 
     const isServer = process.env.IS_SERVER_FLAG ? 'RUN ON SERVER' : 'RUN_ON_CLIENT'
 
@@ -17,12 +15,9 @@ async function callMongoDB()  {
 // children = will be a page or nested layout
 export default async function Page({children,  }) {
 
-
     const mongoData =  await callMongoDB();
     console.log("mongoData")
     console.log(mongoData)
-
-
 
     return (<>
             <h1>Layout</h1>
